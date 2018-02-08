162ec5ed9659904fc4b3453ee9527ea7899210c0
wahom <hwx613@qq.com>
2018-02-08 23:20:05 +0800

    add app01a project

---
+++ app01a/src/MyServlet.java
@@ -0,0 +1,39 @@
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "MyServlet", urlPatterns = {"/my"})
+public class MyServlet implements Servlet {
+    private transient ServletConfig servletConfig;
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException{
+        this.servletConfig = servletConfig;
+    }
+
+    @Override
+    public ServletConfig getServletConfig(){
+        return servletConfig;
+    }
+
+    @Override
+    public String getServletInfo(){
+        return "My Servlet";
+    }
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException{
+        String servletName = servletConfig.getServletName();
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.print("<html><head></head>"
+                + "<body>Hello from " + servletName
+                + "</body></html>");
+    }
+
+    @Override
+    public void destroy(){
+
+    }
+}
---
+++ app01a/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Wa_hom
+  Date: 2018-02-08
+  Time: 22:42
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>My Servlet Test</title>
+  </head>
+  <body>
+    <h2>My Servlet 01</h2>
+  </body>
+</html>
