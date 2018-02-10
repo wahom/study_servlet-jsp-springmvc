3f4cccf6af3292a0637880c9280055860a228e81
wahom <hwx613@qq.com>
2018-02-10 23:34:03 +0800

    add newfolder app01c

---
+++ app01c/src/WelcomeServlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class WelcomeServlet extends HttpServlet {
+    private static final long serialVersionUID = 27126L;
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.print("<html><head></head>" + "<body>Welcome</body></html>");
+    }
+}
