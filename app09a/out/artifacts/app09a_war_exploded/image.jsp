21d70fe5c18eced0555d779fe8afeb25ca7dcce9
wahom <hwx613@qq.com>
2018-03-27 23:58:20 +0800

    add new project app09a

---
+++ app09a/out/artifacts/app09a_war_exploded/image.jsp
@@ -0,0 +1,9 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Wa_hom
+  Date: 2018-03-27
+  Time: 23:27
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<img src='image/logo.png'/>
---
+++ app09a/src/filter/ImageProtectorFilter.java
@@ -0,0 +1,36 @@
+package filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter(filterName = "ImageProtetorFilter", urlPatterns = {
+        "*.png", "*.jpg", "*.gif" })
+public class ImageProtectorFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig)
+            throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request,
+                         ServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+        System.out.println("ImageProtectorFilter");
+        HttpServletRequest httpServletRequest =
+                (HttpServletRequest) request;
+        String referrer = httpServletRequest.getHeader("referer");
+        System.out.println("referrer:" + referrer);
+        if (referrer != null) {
+            filterChain.doFilter(request, response);
+        } else {
+            throw new ServletException("Image not available");
+        }
+    }
+}
