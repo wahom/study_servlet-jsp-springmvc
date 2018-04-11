67faedc83be806e78140f26d357b69e60f3442b7
wahom <hwx613@qq.com>
2018-04-12 00:03:45 +0800

    add new project app12d

---
+++ app12d/src/servlet/Servlet1.java
@@ -0,0 +1,23 @@
+package servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.ServletSecurity;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = { "/servlet1" })
+@ServletSecurity(@HttpConstraint(rolesAllowed = "manager"))
+public class Servlet1 extends HttpServlet {
+
+    private static final long serialVersionUID = -11420L;
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/1.jsp");
+        dispatcher.forward(request,  response);
+    }
+}
