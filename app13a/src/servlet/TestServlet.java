00aa18ac700520172255396bd7ec4418a55d2c70
wahom <hwx613@qq.com>
2018-04-13 00:14:08 +0800

    add new project app13a

---
+++ app13a/src/servlet/TestServlet.java
@@ -0,0 +1,21 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = { "/test" })
+public class TestServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 112220L;
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("Test");
+    }
+}
